b244e1eb63b0fc7e21eaa1c6e58834fb604dc9ff
Roshaan Singh <swaggsikh@gmail.com>
2021-05-21 00:04:15 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,13 +1,8 @@
 def my_collect(collection)
   i = 0
   while i < 0 
-  yield array[i]
+  yield array[i].split(" ").first
   i += 1 
 end
 collection
 end
-
-my_collect(collection) do |student|
-  name.split(" ").first 
-end
-  
